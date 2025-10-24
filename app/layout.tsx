a8ef7bca00475b7ddc6f25b8b173e93196ed7767
[Rasmus Krogh-Andersen] <rasmus.kroghandersen@gmail.com>
2025-10-24 11:45:59 +0200

    added a personolized favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ActiveProvider } from "./context/ActiveContext";
 import { Playfair_Display, Merriweather } from "next/font/google";
+import type { Metadata } from "next";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -14,6 +15,14 @@ const merriweather = Merriweather({
   weight: ["300", "400", "700", "900"],
   variable: "--font-merriweather",
 });
+
+export const metadata: Metadata = {
+  title: "Erik Schoeffler",
+  description: "Official website of Erik Schoeffler",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 export default function RootLayout({
   children,

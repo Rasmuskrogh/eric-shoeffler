f5400688091e290593afa468434284bac145d237
[Rasmus Krogh-Andersen] <rasmus.kroghandersen@gmail.com>
2026-01-23 13:44:32 +0100

    fixing hero image

--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -66,7 +66,7 @@ const Hero = ({
             fill
             priority
             quality={95}
-            unoptimized={false}
+            unoptimized={true}
           />
         </figure>
       </div>
